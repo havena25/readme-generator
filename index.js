df021d65a3bb4da00eae027c976c0b0d0ef2fc34
Alia Haven <aliahavenart@gmail.com>
2022-02-26 14:20:24 +0000

    added the require function and set up questions

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require ('./utils/generateMarkdown');
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {name: 'username',
+    message: 'What is your Github username?',
+    type: 'input'},
+    {name: 'email',
+    message: 'What is your e-mail?',
+    type: 'input'},
+    {name: 'title', 
+    message: 'What is the title of your application?',
+    type: 'input'},
+    {name: 'description',
+    message: 'What does this application do?',
+    type: 'input'},
+    {name: 'usage',
+    message: 'How do I use this application?',
+    type: 'input'},
+    {name: 'contributing',
+    message: 'How do I contribute to this application?',
+    type: 'input'},
+    {name: 'testing',
+    message: 'How do I test this application?',
+    type: 'input'},
+    {name: 'license',
+    message: 'Select kind of license for this application:',
+    type: 'list',
+    choices: ["Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", 'BSD 2-clause "Simplified" license', 'BSD 3-clause "New" or "Revised" license', "BSD 3-clause Clear license", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Educational Community License v2.0", "Eclipse Public License 1.0", "Eclipse Public License 2.0", "European Union Public License 1.1", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU General Public License v3.0", "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v3.0", "ISC", "LaTeX Project Public License v1.3c", "Microsoft Public License", "MIT", "Mozilla Public License 2.0", "Open Software License 3.0", "PostgreSQL License", "SIL Open Font License 1.1", "University of Illinois/NCSA Open Source License", "The Unlicense", "zLib License"]},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
