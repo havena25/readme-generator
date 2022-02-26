8f20f80d5ad0e9a3fbd8604c9aa592df5b93d1a5
Alia Haven <aliahavenart@gmail.com>
2022-02-26 16:30:45 +0000

    question promts  working +  logging on console

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const generateMarkdown = require ('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 
-const questions = [
+function userInput(){
+    return inquirer.prompt( [
     {name: 'username',
     message: 'What is your Github username?',
     type: 'input'},
@@ -33,13 +34,16 @@ const questions = [
     message: 'Select kind of license for this application:',
     type: 'list',
     choices: ["Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", 'BSD 2-clause "Simplified" license', 'BSD 3-clause "New" or "Revised" license', "BSD 3-clause Clear license", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Educational Community License v2.0", "Eclipse Public License 1.0", "Eclipse Public License 2.0", "European Union Public License 1.1", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU General Public License v3.0", "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v3.0", "ISC", "LaTeX Project Public License v1.3c", "Microsoft Public License", "MIT", "Mozilla Public License 2.0", "Open Software License 3.0", "PostgreSQL License", "SIL Open Font License 1.1", "University of Illinois/NCSA Open Source License", "The Unlicense", "zLib License"]},
-];
+])};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    let answers = await userInput();
+    console.log(answers);
+}
 
 // Function call to initialize app
 init();
